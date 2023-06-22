f67f100c7d1d199c4ac15605c7947ceb018fe78f
jungjieun <je_eun2735@naver.com>
2023-06-23 01:11:27 +0900

    setup

---
+++ my-app/src/connect.tsx
@@ -0,0 +1,35 @@
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+
+// function MyComponent() {
+//   const [data, setData] = useState<any[]>([]);
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios.get('http://localhost:3001/api/data');
+//       setData(response.data);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       {data.length > 0 ? (
+//         <ul>
+//           {data.map((item: any) => (
+//             <li key={item.id}>{item.name}</li>
+//           ))}
+//         </ul>
+//       ) : (
+//         <p>Loading...</p>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default MyComponent;
