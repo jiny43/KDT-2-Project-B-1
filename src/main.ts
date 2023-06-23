d363b2f059679587be08baa82d795f995a00bb3c
BlahBlah <leesemin222@gmail.com>
2023-06-23 16:10:27 +0900

    expres 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import path from 'path';
+import express, { Request, Response } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  await app.init();
+  server.use(express.static(path.join(__dirname, '..', 'dist', 'react')));
+  app.getHttpAdapter().get('*', (_req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '..', 'react', 'index.html'));
+  });
   await app.listen(3000);
 }
 bootstrap();
