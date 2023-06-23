72147a646d8c6fd9f0963d3f11b96841d93738cd
BlahBlah <leesemin222@gmail.com>
2023-06-23 15:25:59 +0900

    module 파일 설정완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { reactModule } from './client.module';
 
 @Module({
-  imports: [],
+  imports: [reactModule],
   controllers: [AppController],
   providers: [AppService],
 })
