a4fd716cfd2dcc0781c5307135c2e8d7d6284749
jungjieun <je_eun2735@naver.com>
2023-06-23 14:42:15 +0900

    .

---
+++ src/app.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+
+@Controller('*')
+export class AppController {
+  @Get()
+  getIndex(@Res() res: Response) {
+    res.sendFile('index.html', { root: './client/build' });
+  }
+}
