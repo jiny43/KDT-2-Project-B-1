6b44feec9eef96012e3a24a12547f0db2a379c4d
BlahBlah <leesemin222@gmail.com>
2023-06-26 16:04:22 +0900

    main.ts 필요없는 부분 삭제

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { join } from 'path';
-import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  app.use(express.static(join(__dirname, '..', '..', '/frontend')));
 }
 bootstrap();
