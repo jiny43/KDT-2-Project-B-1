0bccde0e368df16fadaf886f2697b698a4f7aa44
BlahBlah <leesemin222@gmail.com>
2023-06-26 16:40:20 +0900

    최신화 완료

--- react/index.tsx
+++ react/index.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 const App = () => {
   return <h1>Hello World Project-B-1-Team</h1>;
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error('Root element not found');
+}
