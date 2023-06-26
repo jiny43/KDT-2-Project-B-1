58b146585e65ab9bb616bc6e5ab06ed65164bd14
BlahBlah <leesemin222@gmail.com>
2023-06-26 17:28:55 +0900

    다른 컴포넌트를 여기에 입력하면 됨

--- react/index.tsx
+++ react/index.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
+import KakaoMap from './view/kakaoMap';
 
 const App = () => {
-  return <h1>Hello World Project-B-1-Team</h1>;
+  return (
+    <>
+      <KakaoMap />
+    </>
+  );
 };
 
 const rootElement = document.getElementById('root');
