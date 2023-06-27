0d4b47b8d275e2f66a6337e62f9ec94f0c6b588c
BlahBlah <leesemin222@gmail.com>
2023-06-27 11:44:57 +0900

    리액트 라우터 추가

--- react/index.tsx
+++ react/index.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Start from './view/start';
 
-const App = () => {
-  return <h1>Hello World Project-B-1-Team</h1>;
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Start />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = createRoot(rootElement);
-  root.render(<App />);
+  root.render(<Router />);
 } else {
   console.error('Root element not found');
 }
---
+++ react/view/start.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Start = () => {
+  return (
+    <div>
+      <h1> Project-B-1 </h1>
+      <h3> 행복한 세상의 다람쥐들 </h3>
+      <h5> 먹보의 여행 </h5>
+    </div>
+  );
+};
+
+export default Start;
