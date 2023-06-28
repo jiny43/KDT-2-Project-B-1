456cb20eebdba5223eefa0d285664f5552c320e2
BlahBlah <leesemin222@gmail.com>
2023-06-28 17:28:54 +0900

    fetch를 사용한 데이터 받기

--- cute/.eslintrc.js
+++ cute/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
     // rules에 다음과 같이 추가
     'prettier/prettier': [
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('inputData')
+  handleInputData(@Body('value') value: string): { status: string } {
+    try {
+      console.log('입력한 input 정보 : ', value);
+      return { status: '정보를 성공적으로 받아왔습니다.' };
+    } catch (error) {
+      console.error('정보를 받아오는데 실패했습니다.', error);
+    }
   }
 }
