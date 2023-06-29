83c07aa133cc4f4aca106c7148f15f89fc838b31
BlahBlah <leesemin222@gmail.com>
2023-06-29 15:35:53 +0900

    저장

--- cute/App.tsx
+++ cute/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 // import InputTest from './View/getExample';
 import Map from './View/map';
+import GoogleMap from './View/GoogleMap';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
       <Map></Map>
+      <GoogleMap />
     </SafeAreaView>
   );
 }
