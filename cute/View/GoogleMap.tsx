22154e10293567475ebbcabd1135517e8cae824d
BlahBlah <leesemin222@gmail.com>
2023-06-29 17:46:01 +0900

    지도 띄우기

--- cute/View/GoogleMap.tsx
+++ cute/View/GoogleMap.tsx
@@ -1,15 +1,22 @@
-import MapView from 'react-native-maps';
+import {View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const GoogleMap = () => {
   return (
-    <MapView
-      initialRegion={{
-        latitude: 36.36,
-        longitude: 127.38,
-        latitudeDelta: 36.0,
-        longitudeDelta: 127.0,
-      }}
-    />
+    <>
+      <View>
+        <MapView
+          style={{width: '100%', height: '100%'}}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 36.36,
+            longitude: 127.38,
+            latitudeDelta: 36.0,
+            longitudeDelta: 127.0,
+          }}
+        />
+      </View>
+    </>
   );
 };
 
