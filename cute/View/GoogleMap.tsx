ac81e8ec58f9a70c290623219f7d5ed5530c9ed0
BlahBlah <leesemin222@gmail.com>
2023-06-30 14:30:42 +0900

    위도 경도 좌표 6대광역시 + 경기도 정보 json으로 저장

--- cute/View/GoogleMap.tsx
+++ cute/View/GoogleMap.tsx
@@ -1,5 +1,7 @@
 import {View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MetroCoord from '../model/MetropolitanCoordinate.json';
+import {useState} from 'react';
 
 const GoogleMap = () => {
   return (
@@ -8,12 +10,7 @@ const GoogleMap = () => {
         <MapView
           style={{width: '100%', height: '100%'}}
           provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: 36.3575,
-            longitude: 127.3808,
-            latitudeDelta: 0.01,
-            longitudeDelta: 0.005,
-          }}
+          initialRegion={}
         />
       </View>
     </>
