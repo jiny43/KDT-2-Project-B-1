2ec0fdd2c6113a3b51106531e500b61406d5912a
BlahBlah <leesemin222@gmail.com>
2023-07-01 15:18:40 +0900

    임시 모달창 생성

--- cute/App.tsx
+++ cute/App.tsx
@@ -3,12 +3,14 @@ import {SafeAreaView} from 'react-native';
 // import InputTest from './View/getExample';
 import Map from './View/map';
 import GoogleMap from './View/GoogleMap';
+import OpenModalToClickMarker from './View/openModalToClickMarker';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
       {/* <Map></Map> */}
-      <GoogleMap />
+      {/* <GoogleMap /> */}
+      <OpenModalToClickMarker />
     </SafeAreaView>
   );
 }
