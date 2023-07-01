35303019272455a60cefc080c01d8bc314650ce0
BlahBlah <leesemin222@gmail.com>
2023-07-01 14:36:38 +0900

    네이티브 클릭이벤트는 맵 전체클릭시밖에 안되므로 Marker내의 onPress를 사용함

--- cute/View/GoogleMap.tsx
+++ cute/View/GoogleMap.tsx
@@ -17,26 +17,23 @@ const GoogleMap = () => {
   return (
     <>
       <View>
-        <TouchableOpacity
-          ref={ref}
-          onPress={() => {
-            Alert.alert('clicked!');
-          }}>
-          <MapView
-            style={{width: '100%', height: '100%'}}
-            provider={PROVIDER_GOOGLE}
-            initialRegion={addLatLngDate(MetroCoord.daejeon, latLngDeltaData)}>
-            <Marker
-              key={Object.keys(MetroCoord.daejeon)[0]}
-              coordinate={MetroCoord.daejeon}
-              description={'대전 소보로빵'}>
-              <Image
-                source={require('../Img/Daejeon_Twigim-soboro-bread.png')}
-                style={{width: 70, height: 70}}
-              />
-            </Marker>
-          </MapView>
-        </TouchableOpacity>
+        <MapView
+          style={{width: '100%', height: '100%'}}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={addLatLngDate(MetroCoord.daejeon, latLngDeltaData)}>
+          <Marker
+            key={Object.keys(MetroCoord.daejeon)[0]}
+            coordinate={MetroCoord.daejeon}
+            description={'대전 소보로빵'}
+            onPress={() => {
+              Alert.alert('클릭해봐라 마!');
+            }}>
+            <Image
+              source={require('../Img/Daejeon_Twigim-soboro-bread.png')}
+              style={{width: 70, height: 70}}
+            />
+          </Marker>
+        </MapView>
       </View>
     </>
   );
