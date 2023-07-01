26150030cc8d7547321f02f06604353df9f0a9d5
BlahBlah <leesemin222@gmail.com>
2023-07-01 14:35:46 +0900

    네이티브 useRef를 사용해서 클릭이벤트 생성 테스트

--- cute/View/GoogleMap.tsx
+++ cute/View/GoogleMap.tsx
@@ -1,9 +1,10 @@
-import {Image, View} from 'react-native';
+import {Alert, Image, TouchableOpacity, View} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import MetroCoord from '../model/MetropolitanCoordinate.json';
 import addLatLngDate, {
   latLngDeltaDataType,
 } from '../model/mapviewInitialRegionData';
+import {useRef} from 'react';
 
 const GoogleMap = () => {
   const latLngDeltaData: latLngDeltaDataType = {
@@ -11,26 +12,33 @@ const GoogleMap = () => {
     longitudeDelta: 0.5,
   };
 
+  const ref = useRef<TouchableOpacity>(null);
+
   return (
     <>
       <View>
-        <MapView
-          style={{width: '100%', height: '100%'}}
-          provider={PROVIDER_GOOGLE}
-          initialRegion={addLatLngDate(MetroCoord.daejeon, latLngDeltaData)}>
-          <Marker
-            key={Object.keys(MetroCoord.daejeon)[0]}
-            coordinate={MetroCoord.daejeon}
-            description={'대전 소보로빵'}>
-            <Image
-              source={require('../Img/Daejeon_Twigim-soboro-bread.png')}
-              style={{width: 70, height: 70}}
-            />
-          </Marker>
-        </MapView>
+        <TouchableOpacity
+          ref={ref}
+          onPress={() => {
+            Alert.alert('clicked!');
+          }}>
+          <MapView
+            style={{width: '100%', height: '100%'}}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={addLatLngDate(MetroCoord.daejeon, latLngDeltaData)}>
+            <Marker
+              key={Object.keys(MetroCoord.daejeon)[0]}
+              coordinate={MetroCoord.daejeon}
+              description={'대전 소보로빵'}>
+              <Image
+                source={require('../Img/Daejeon_Twigim-soboro-bread.png')}
+                style={{width: 70, height: 70}}
+              />
+            </Marker>
+          </MapView>
+        </TouchableOpacity>
       </View>
     </>
   );
 };
-GoogleMap();
 export default GoogleMap;
