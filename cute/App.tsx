8f5be3820ce8a706afc1c08a9c59664b335584db
BlahBlah <leesemin222@gmail.com>
2023-07-01 15:37:23 +0900

    저장

--- cute/App.tsx
+++ cute/App.tsx
@@ -9,8 +9,8 @@ const App: React.FC = () => {
   return (
     <SafeAreaView>
       {/* <Map></Map> */}
-      {/* <GoogleMap /> */}
-      <OpenModalToClickMarker />
+      <GoogleMap />
+      {/* <OpenModalToClickMarker /> */}
     </SafeAreaView>
   );
 };
