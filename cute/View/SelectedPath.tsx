dd467a852a17d5b39e908103bf024d6af596a741
jungjieun <je_eun2735@naver.com>
2023-07-02 15:56:02 +0900

    주석추가

--- cute/View/SelectedPath.tsx
+++ cute/View/SelectedPath.tsx
@@ -3,9 +3,10 @@ import { Text } from "react-native";
 
 //googlemap 에 prop 으로 전달해주기 !
 type SelectedPathProps = {
-  path: string;
+  path: string; //문자열 타입 
 };
 
+//SelectedPath 컴포넌트는 'path' prop을 받아서 Text 컴포넌트로 렌더링해준다.
 const SelectedPath = ({ path }: SelectedPathProps) => {
   return (
     <Text>{path}</Text>
