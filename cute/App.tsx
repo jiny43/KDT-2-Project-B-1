4ce02e155d06fa0836f0b214733ec7570504ff29
jungjieun <je_eun2735@naver.com>
2023-07-02 16:51:27 +0900

    safeAreaView 스타일 수정

--- cute/App.tsx
+++ cute/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import Map from './View/map';
-import {SafeAreaView, StyleSheet} from 'react-native';
-// import InputTest from './View/getExample';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import GoogleMap from './View/GoogleMap';
-import SelectedPath from './View/SelectedPath';
+import RecommendedPath from './View/RecommendedPath';
 
 const App: React.FC = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <GoogleMap />
+      <RecommendedPath />
     </SafeAreaView>
   );
 };
 
-// 최상위 부모요소의 설정없이는 안먹어서 부득이하게..
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
+
 export default App;
--- cute/View/RecommendedPath.tsx
+++ cute/View/RecommendedPath.tsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
-
-const RecommendedPath =() => {
+const RecommendedPath = () => {
   return (
-   
+    <View style={styles.container}>
+      <Text style={styles.title}>내비 추천</Text>
+      <Text style={styles.duration}>45분</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  duration: {
+    fontSize: 16,
+  },
+});
 
 export default RecommendedPath;
