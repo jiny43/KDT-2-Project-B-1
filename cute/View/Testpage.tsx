a1c9def6ac3df16f57fc006c3f37d3ebc0aa689f
jungjieun <je_eun2735@naver.com>
2023-07-02 17:58:55 +0900

    로직 짜기 , 라우트 해야함

---
+++ cute/View/Testpage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const testpage = () => {
+  return (
+    <View>    
+        <Text>안녕하세요!</Text>
+      
+    </View>
+  );
+}
+
+
+export default testpage;
