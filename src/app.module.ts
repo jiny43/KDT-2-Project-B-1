334dca5ac65e147290f5865c6cf09b0bcb4dcb3f
jungjieun <je_eun2735@naver.com>
2023-07-03 19:55:29 +0900

    예상 운전 거리 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KakaoApiService } from './kakao-api/kakao-api.service';
 import { KakaoApiController } from './kakao-api/kakao-api.controller';
+// import { KakaoApiDirection } from './kakao-api/kakao-api-direction.service';
 
 @Module({
   imports: [],
--- src/kakao-api/kakao-api.service.ts
+++ src/kakao-api/kakao-api.service.ts
@@ -50,7 +50,7 @@ export class KakaoApiService {
   private origin = '127.3937,36.3399'; // 출발지
   private destination = '127.4332,36.3521'; // 목적지
 
-  async getDirections(): Promise<number> {
+  async getDuration(): Promise<any> {
     const url = `https://apis-navi.kakaomobility.com/v1/directions?origin=${this.origin}&destination=${this.destination}`;
     const headers = {
       Authorization: `KakaoAK ${this.kakao_api_key}`,
@@ -60,9 +60,10 @@ export class KakaoApiService {
     try {
       const response = await axios.get(url, { headers });
       const data = response.data; // 응답 데이터
-
       const duration = data.routes[0].sections[0].duration;
-      return duration;
+      const distance = data.routes[0].sections[0].distance;
+      console.log(duration, distance);
+      return { duration, distance }; // 객체로 값을 반환);
     } catch (error) {
       console.error(`Error: ${error}`);
       return null;
