aa057449f8e894771d99c2fb58f97fa0c237476a
BlahBlah <leesemin222@gmail.com>
2023-07-03 16:21:14 +0900

    모달 컴포넌트로 3개 띄우기

--- cute/View/TouchableOpacityImageText.tsx
+++ cute/View/TouchableOpacityImageText.tsx
@@ -1,4 +1,54 @@
 import React from 'react';
-import {} from 'react-native';
+import {
+  Image,
+  ImageProps,
+  Modal,
+  StyleSheet,
+  Text,
+  TextProps,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from 'react-native';
 
-const modalSelectThree = () => {};
+interface ModalSelectThreeProps extends TouchableOpacityProps {
+  imageProps: ImageProps;
+  textProps: TextProps;
+  children: React.ReactNode;
+}
+
+const ModalSelectThree = (/* props: ModalSelectThreeProps */) => {
+  // const {imageProps, textProps, children, ...touchableOpacityProps} = props;
+  const duruchigiRequire = require('../Img/Daejeon_Duruchigi.png');
+  return (
+    <TouchableOpacity>
+      <Image
+        source={duruchigiRequire}
+        alt="두부두루치기"
+        style={ModalStyle.imgStyle}
+      />
+      <Text style={ModalStyle.textStyle}>두부두루치기</Text>
+    </TouchableOpacity>
+  );
+};
+
+const ModalStyle = StyleSheet.create({
+  width70: {
+    width: '70%',
+  },
+  height100: {
+    height: '100%',
+  },
+  backgroundColor: {
+    backgroundColor: '#fafafa',
+  },
+  imgStyle: {
+    width: '30%',
+    height: '30%',
+    borderRadius: 100,
+  },
+  textStyle: {
+    flex: 1,
+  },
+});
+
+export default ModalSelectThree;
