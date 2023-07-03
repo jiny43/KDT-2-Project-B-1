0176f0aea4ed94cdea6ecf9bd80efea56a6ac407
HaekyungS <haekyungs95@gmail.com>
2023-07-03 13:35:21 +0900

    내 위치 기준 주차장 검색하여 마커 찍기~

--- cute/View/keywordMap.tsx
+++ cute/View/keywordMap.tsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
-import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import React, {useEffect, useState} from 'react';
+import {View, Text} from 'react-native';
+import MapView, {Callout, Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
 const API_KEY = 'AIzaSyBxMsKTMvDP6CxDuDjIz9PIln46JK87kro';
 const YOUR_KEYWORD = 'parking'; // 원하는 키워드로 변경하세요.
 
-export const ParkingMap=()=> {
+export const ParkingMap = () => {
   const [latitude, setLatitude] = useState<number | null>(null);
   const [longitude, setLongitude] = useState<number | null>(null);
   const [places, setPlaces] = useState<any[]>([]);
 
   useEffect(() => {
+    // watchPosition 은 위치가 바뀔 때마다 자동으로 호출할 처리기 함수 사용
     const watchID = Geolocation.watchPosition(
       position => {
-        const { latitude, longitude } = position.coords;
+        const {latitude, longitude} = position.coords;
         setLatitude(latitude);
         setLongitude(longitude);
       },
@@ -36,13 +37,13 @@ export const ParkingMap=()=> {
     async function fetchPlaces() {
       if (latitude !== null && longitude !== null) {
         try {
-          console.log(latitude, longitude)
+          console.log(latitude, longitude);
           const response = await fetch(
             `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&keyword=${YOUR_KEYWORD}&key=${API_KEY}`,
           );
           const jsonResponse = await response.json();
 
-          console.log(jsonResponse)
+          console.log(jsonResponse.results);
           if (jsonResponse.status === 'OK') {
             setPlaces(jsonResponse.results);
           }
@@ -58,35 +59,30 @@ export const ParkingMap=()=> {
   return (
     <>
       {latitude && longitude && (
-        <View style={{ flex: 1 }}>
-          <MapView
-            // provider={PROVIDER_GOOGLE}
-            style={{ width:"100%",height:"100%",flex: 1 }}
-            initialRegion={{
-              latitude,
-              longitude,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-          >
-            {places.map((place, index) => (
-              <Marker
-                key={index}
-                coordinate={{
-                  latitude: place.geometry.location.lat,
-                  longitude: place.geometry.location.lng,
-                }}
-              >
-                <Callout>
-                  <View>
-                    <Text>{JSON.stringify(place)}</Text>
-                  </View>
-                </Callout>
-              </Marker>
-            ))}
-          </MapView>
-        </View>
+        <MapView
+          style={{width: '100%', height: '100%'}}
+          initialRegion={{
+            latitude: Number(latitude),
+            longitude: Number(longitude),
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+          {places.map((place, index) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: place.geometry.location.lat,
+                longitude: place.geometry.location.lng,
+              }}>
+              <Callout>
+                <View>
+                  <Text>{JSON.stringify(place)}</Text>
+                </View>
+              </Callout>
+            </Marker>
+          ))}
+        </MapView>
       )}
     </>
   );
-}
+};
