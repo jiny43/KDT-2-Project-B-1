eaf402a51a6b3a8cb01943048a74bc7a9ec39736
leesemin <leesemin222@gmail.com>
2023-07-05 01:58:42 +0900

    광역시 마커 컴포넌트로 분리

--- cute/App.tsx
+++ cute/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import RecommendedPath from './jieunView/RecommendedPath';
 import Map from './jieunView/map';
+import GoogleMap from './View/GoogleMap';
 
 function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Map></Map>
-      <RecommendedPath></RecommendedPath>
+      {/* <Map></Map> */}
+      {/* <RecommendedPath></RecommendedPath> */}
+      <GoogleMap />
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
