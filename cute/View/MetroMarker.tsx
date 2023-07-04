7e2ed60a85f61dd40fbe8cfaa35fabc3e9a629c7
leesemin <leesemin222@gmail.com>
2023-07-05 02:21:22 +0900

    마커에 필요한 광역시 리터럴 작성

--- cute/View/MetroMarker.tsx
+++ cute/View/MetroMarker.tsx
@@ -2,16 +2,36 @@ import React from 'react';
 import {Marker} from 'react-native-maps';
 import MetroCoord from '../model/MetropolitanCoordinate.json';
 import {Image} from 'react-native';
+import {MetroCityList} from './TouchableOpacityImageText';
 
-const MetroMarker = ({openModal}: {openModal: () => void}) => {
+const MetroMarker = ({
+  openModal,
+}: // region,
+{
+  openModal: () => void;
+  // region: string;
+}) => {
+  type MetroMarkerType = {
+    [key in MetroCityList]: [string, any];
+  };
+
+  const MetroRegionInfo: MetroMarkerType = {
+    busan: ['부산 돼지국밥', require('../Img/Busan_Dwaeji-gukbap.png')],
+    daegu: ['대구 막창', require('../Img/Daegu_Makchang.png')],
+    daejeon: ['대전 빵', require('../Img/Daejeon_Twigim-soboro-bread.png')],
+    gangwondo: ['강원도 닭강정', require('../Img/Gangwondo_Dakgangjeong.png')],
+    gwangju: ['광주 오리탕', require('../Img/Gwangju_Ori-tang.png')],
+    incheon: ['인천 홍어', require('../Img/Incheon_Hongeo.png')],
+    ulsan: ['울산 고래고기', require('../Img/Ulsan_Gorae-gogi.png')],
+  };
   return (
     <Marker
       key={Object.keys(MetroCoord.daejeon)[0]}
       coordinate={MetroCoord.daejeon}
-      description={'대전 소보로빵'}
+      description={MetroRegionInfo.daejeon[0]}
       onPress={openModal}>
       <Image
-        source={require('../Img/Daejeon_Twigim-soboro-bread.png')}
+        source={MetroRegionInfo.daejeon[1]}
         style={{width: 70, height: 70}}
       />
     </Marker>
