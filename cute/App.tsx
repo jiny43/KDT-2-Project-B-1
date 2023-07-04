392c6ee82190e5c354cc1d27275f581e58a5ca42
김종윤 <kimjongyoon@gimjong-yun-ui-MacBookAir.local>
2023-07-04 15:45:46 +0900

    네비게이션 이동 예시

--- cute/App.tsx
+++ cute/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-// import InputTest from './View/getExample';
-// import GoogleMap from './View/GoogleMap';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import OpenModalToClickMarker from './View/openModalToClickMarker';
 import Map from './View/yoone';
-import Map2 from './View/map';
+import A from './View/test';
 
+// Create the application stack
+const Stack = createStackNavigator();
 const App: React.FC = () => {
   return (
-    <SafeAreaView>
-      <Map />
-      {/* <Map2 /> */}
-      {/* <GoogleMap /> */}
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="A">
+          <Stack.Screen name="A" component={A} />
+          <Stack.Screen name="Map" component={Map} />
+          {/* Add more screens as needed */}
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 };
