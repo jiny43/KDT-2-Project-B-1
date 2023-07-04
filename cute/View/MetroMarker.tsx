cff94741d9fa5de9bf7532874a3db7f606d27604
leesemin <leesemin222@gmail.com>
2023-07-05 02:36:57 +0900

    openmodal로 정보 불러와지나 테스트 중

--- cute/View/MetroMarker.tsx
+++ cute/View/MetroMarker.tsx
@@ -8,21 +8,33 @@ const MetroMarker = ({
   openModal,
 }: // region,
 {
-  openModal: () => void;
+  openModal: (regionInfo: string) => void;
   // region: string;
 }) => {
   type MetroMarkerType = {
-    [key in MetroCityList]: [string, any];
+    [key in MetroCityList]: [string, string, any];
   };
 
   const MetroRegionInfo: MetroMarkerType = {
-    busan: ['부산 돼지국밥', require('../Img/Busan_Dwaeji-gukbap.png')],
-    daegu: ['대구 막창', require('../Img/Daegu_Makchang.png')],
-    daejeon: ['대전 빵', require('../Img/Daejeon_Twigim-soboro-bread.png')],
-    gangwondo: ['강원도 닭강정', require('../Img/Gangwondo_Dakgangjeong.png')],
-    gwangju: ['광주 오리탕', require('../Img/Gwangju_Ori-tang.png')],
-    incheon: ['인천 홍어', require('../Img/Incheon_Hongeo.png')],
-    ulsan: ['울산 고래고기', require('../Img/Ulsan_Gorae-gogi.png')],
+    busan: [
+      'busan',
+      '부산 돼지국밥',
+      require('../Img/Busan_Dwaeji-gukbap.png'),
+    ],
+    daegu: ['daegu', '대구 막창', require('../Img/Daegu_Makchang.png')],
+    daejeon: [
+      'daejeon',
+      '대전 빵',
+      require('../Img/Daejeon_Twigim-soboro-bread.png'),
+    ],
+    gangwondo: [
+      'gangwondo',
+      '강원도 닭강정',
+      require('../Img/Gangwondo_Dakgangjeong.png'),
+    ],
+    gwangju: ['gwangju', '광주 오리탕', require('../Img/Gwangju_Ori-tang.png')],
+    incheon: ['incheon', '인천 홍어', require('../Img/Incheon_Hongeo.png')],
+    ulsan: ['ulsan', '울산 고래고기', require('../Img/Ulsan_Gorae-gogi.png')],
   };
 
   const regionList: MetroCityList[] = [
@@ -42,10 +54,10 @@ const MetroMarker = ({
           <Marker
             key={Object.keys(MetroCoord[ele])[0]}
             coordinate={MetroCoord[ele]}
-            description={MetroRegionInfo[ele][0]}
-            onPress={openModal}>
+            description={MetroRegionInfo[ele][1]}
+            onPress={() => openModal(MetroRegionInfo[ele][0])}>
             <Image
-              source={MetroRegionInfo[ele][1]}
+              source={MetroRegionInfo[ele][2]}
               style={{width: 70, height: 70}}
             />
           </Marker>
