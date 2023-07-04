6899301e95fef273b690031e5edfafe86f79477c
jungjieun <je_eun2735@naver.com>
2023-07-04 21:58:52 +0900

    View폴더 생성

--- cute/App.tsx
+++ cute/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-// import KakaoService from './View/kakaoService';
-// import GoogleMap from './View/GoogleMap';
-import RecommendedPath from './View/RecommendedPath';
-import Map from './View/map';
-// import Parkingbutton from './View/Parkingbutton';
+import RecommendedPath from './jieunView/RecommendedPath';
+import Map from './jieunView/map';
 
 function App() {
   return (
     <SafeAreaView style={styles.container}>
-      {/* <GoogleMap></GoogleMap>
-       */}
       <Map></Map>
-      {/* <Parkingbutton></Parkingbutton>/ */}
       <RecommendedPath></RecommendedPath>
-      {/* <KakaoService /> */}
     </SafeAreaView>
   );
 };
