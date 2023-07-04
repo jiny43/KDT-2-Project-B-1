c6be79df42d4755b2edd031f1e4778e790a31261
jungjieun <je_eun2735@naver.com>
2023-07-04 21:58:52 +0900

    View폴더 생성

--- cute/App.tsx
+++ cute/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import OpenModalToClickMarker from './View/openModalToClickMarker';
-import Map from './View/yoone';
-import A from './View/test';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import RecommendedPath from './jieunView/RecommendedPath';
+import Map from './jieunView/map';
 
-// Create the application stack
-const Stack = createStackNavigator();
-const App: React.FC = () => {
+function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="A">
-          <Stack.Screen name="A" component={A} />
-          <Stack.Screen name="Map" component={Map} />
-          {/* Add more screens as needed */}
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Map></Map>
+      <RecommendedPath></RecommendedPath>
     </SafeAreaView>
   );
 };
