e1e8faa73aeb3a82bdaba4e536689cdcb19648f6
BlahBlah <leesemin222@gmail.com>
2023-07-13 17:35:56 +0900

    크롤링

--- cute/View/openModalToClickMarker.tsx
+++ cute/View/openModalToClickMarker.tsx
@@ -2,6 +2,7 @@ import {Button, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import React from 'react';
 import ModalSelectThree, {MetroCityList} from './TouchableOpacityImageText';
 import RecommendList from './recommendList';
+import coord from '../model/MetropolitanCoordinate.json';
 
 function OpenModalToClickMarker({
   closeModal,
@@ -20,11 +21,13 @@ function OpenModalToClickMarker({
           navigation={navigation}
         />
         <View style={{flex: 3}}>
-          <TouchableOpacity
+          <RecommendList location={coord.daejeon} keywords="두부두루치기" />
+          {/* <TouchableOpacity
             onPress={() => {
               // ('이동하고 싶은 App.tsx에 지정한 name')
               navigation.navigate('map');
-            }}></TouchableOpacity>
+            }}>
+          </TouchableOpacity> */}
         </View>
       </View>
       <View style={ModalStyle.buttonStyle}>
