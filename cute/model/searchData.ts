4fe23ecd4d7e82d845bade6e4adcb2e9a185cb5a
BlahBlah <leesemin222@gmail.com>
2023-07-13 17:35:56 +0900

    크롤링

---
+++ cute/model/searchData.ts
@@ -0,0 +1,30 @@
+import {LatLngTypes} from './mapviewInitialRegionData';
+
+// 비동기로 장소 가져오기.
+async function fetchPlaces(
+  location: LatLngTypes,
+  keywords: string,
+  API_KEY: string,
+) {
+  const {latitude, longitude} = location;
+  // 경도, 위도가 받아진 경우.
+  try {
+    // 아래  fetch URL을 통해 데이터를 가져온다.
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&keyword=${keywords}&key=${API_KEY}`,
+    );
+    // 응답받은 데이터를 변수에 담는다.
+    const jsonResponse = await response.json();
+
+    // 만약 응답받은 데이터 중 status가 OK라면 ( OK인 경우가 제대로 응답받은 것 )
+    if (jsonResponse.status === 'OK') {
+      // 해당 데이터 중 results 안에 있는 위치정보들을 places에 담기.
+      // console.log('요청한 자료 데이터입니다.', jsonResponse.results);
+      return jsonResponse.results;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default fetchPlaces;
