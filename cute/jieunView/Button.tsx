0baadffc9148ef6e0e546eb1ea69fc991739eabe
jungjieun <je_eun2735@naver.com>
2023-07-14 16:38:43 +0900

    오늘 안드로이드스튜디오가 말을 안들어요 진짜..

--- cute/jieunView/Button.tsx
+++ cute/jieunView/Button.tsx
@@ -1,26 +1,33 @@
 import React from "react";
-import { Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, TouchableOpacity, View, Alert } from "react-native";
 
-type ButtonName = {
+interface ButtonProps {
   name: string;
-};
+}
 
-const Button = ({ name }: ButtonName) => {
+const Button: React.FC<ButtonProps> = ({ name }) => {
   return (
-    <TouchableOpacity style={styles.selectedname}>{name}</TouchableOpacity >
+    <View>
+      <TouchableOpacity
+        style={styles.selectedname}
+        onPress={() => Alert.alert('주차장경로로 변경해야함')}
+      >
+        <Text>{name}</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   selectedname: {
-    width:100,
-    height:40,
-    backgroundColor:'white',
-    borderRadius:100,
-    position:"absolute",
-    bottom:190,
-    left:15,
-    fontSize:20,
+    width: 100,
+    height: 35,
+    backgroundColor: 'white',
+    borderRadius: 100,
+    position: "absolute",
+    bottom: 170,
+    left: 15,
+    fontSize: 18,
   },
 });
 
