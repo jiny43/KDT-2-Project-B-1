95c710efe553ac61ed369f770ac62557cb3ffa5a
BlahBlah <leesemin222@gmail.com>
2023-07-14 12:42:51 +0900

    저장

--- cute/View/recommendList.tsx
+++ cute/View/recommendList.tsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {LatLngTypes} from '../model/mapviewInitialRegionData';
 import fetchPlaces from '../model/searchData';
 import {Image, Text, View} from 'react-native';
+import hrefRegex, {PhotosType} from '../model/hrefRegex';
 
 export interface FetchPlacesType {
   business_status: string;
@@ -48,35 +49,18 @@ const RecommendList = ({
     fetchPlaces(location, keywords, API_KEY).then(res => {
       const results = res.map((val: any) => {
         if (val.types?.includes('restaurant') || val.types?.includes('food')) {
-          // const valResult = {
-          //   business_status: val.business_status,
-          //   geometry: val.geometry,
-          //   place_id: val.place_id,
-          //   name: val.name,
-          //   vicinity: val.vicinity,
-          //   photos: val.photos || {
-          //     height: 0,
-          //     width: 0,
-          //     html_attributions: '',
-          //     photo_reference: '',
-          //   },
-          //   rating: val.rating || 0,
-          // };
-          // return valResult;
           return val;
         }
       });
       const resultUndefinedFilter = results.filter(
         (val: any[]) => val !== undefined,
       );
-      // const stringify = JSON.stringify(resultUndefinedFilter, null, 2);
 
-      // setFetchData(stringify);
       setFetchData(resultUndefinedFilter);
     });
   }, []);
 
-  console.log('fetchData', fetchData);
+  console.log('fetchData', fetchData[0].photos);
 
   return (
     <View>
@@ -86,20 +70,21 @@ const RecommendList = ({
             <Image source={val.photos} />
             <Text>{val.name}</Text>
             <Text>영업상태 : {val.business_status}</Text>
-            <Text></Text>
+            <Text>추천 : {val.rating}</Text>
+            {/* <Image src={} */}
           </View>
         );
       })}
-      {/* <Text>{fetchData}</Text> */}
-      {/* <Text>{fetchData[1].business_status}</Text>
-        <Text>{fetchData[1].vicinity}</Text>
-        <Text>{fetchData[1].rating}</Text>
-        <Text>{fetchData[1].geometry.location.latitude}</Text>
-        <Text>{fetchData[1].geometry.location.longitude}</Text>
-        <Text>{fetchData[1].photos?.height}</Text>
-        <Text>{fetchData[1].photos?.width}</Text>
-        <Text>{fetchData[1].photos?.html_attributions}</Text>
-        <Text>{fetchData[1].photos?.photo_reference}</Text> */}
+      {/* <Text>{fetchData}</Text> 
+          <Text>{fetchData[1].business_status}</Text>
+          <Text>{fetchData[1].vicinity}</Text>
+          <Text>{fetchData[1].rating}</Text>
+          <Text>{fetchData[1].geometry.location.latitude}</Text>
+          <Text>{fetchData[1].geometry.location.longitude}</Text>
+          <Text>{fetchData[1].photos?.height}</Text>
+          <Text>{fetchData[1].photos?.width}</Text>
+          <Text>{fetchData[1].photos?.html_attributions}</Text>
+          <Text>{fetchData[1].photos?.photo_reference}</Text> */}
     </View>
   );
 };
